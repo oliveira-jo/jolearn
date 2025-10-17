18aeaf2bbe75d05ecd64be8b3ba9ce9ac9abc247
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2025-10-17 16:04:55 -0300

    Create project jolearn

---
+++ backend/src/main/java/com/devjoliveira/jolearn/JolearnApplication.java
@@ -0,0 +1,13 @@
+package com.devjoliveira.jolearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JolearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JolearnApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devjoliveira/jolearn/JolearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjoliveira.jolearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JolearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
