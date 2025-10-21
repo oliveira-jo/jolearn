1062d49bca423db18d39c2af572c9dcfcc8e0909
devjoliveira <macpro@MacBook-Pro-de-MacPro.local>
2025-10-21 20:17:07 -0300

    Refact offer timestempo without times zone

--- backend/src/main/java/com/devjoliveira/jolearn/entities/Notification.java
+++ backend/src/main/java/com/devjoliveira/jolearn/entities/Notification.java
@@ -2,6 +2,7 @@ package com.devjoliveira.jolearn.entities;
 
 import java.time.Instant;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,6 +19,8 @@ public class Notification {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String text;
+
+  @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
   private Instant moment;
   private boolean read;
   private String route;
